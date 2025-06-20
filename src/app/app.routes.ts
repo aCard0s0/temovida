a221e894d9f9aa3d25b2a781d51e1f7a8d98fdcc
André Cardoso <andre.fmarques.cardoso@gmail.com>
2025-06-20 17:01:43 +0100

    Deploy to GitHub Pages

--- src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import {HomeComponent} from './home/home';
-import {LandingComponent} from './landing/landing';
-
-export const routes: Routes = [
-  { path: '', component: LandingComponent },
-  { path: 'home', component: HomeComponent },
-];
